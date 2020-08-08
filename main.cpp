932b3a4867f998f69d9f7e0d53580ab4329db18c
Ryan <rldoan@ucla.edu>
2020-08-08 10:37:47 -0700

    correct off-by-one error

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	cout << "Loop to print number from 1 to 10..." << endl;
+	
+	for(int i = 0; i < 10; i++){
+		cout << i << " ";
+	}
+	
+	cout << "\n\n Ending program..." << endl;
+	
+	return 0;
+}
